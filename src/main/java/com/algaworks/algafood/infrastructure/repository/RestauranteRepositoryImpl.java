588b60632ee3e938a25ccdff1ff9bbb862b1153b
Yuri Santos <yurimax322@gmail.com>
2022-12-10 23:15:12 -0300

    Adicionando método.

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -30,6 +32,9 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	 */
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		
+		Integer[] p = new Integer[1];
+		
 		/*
 		 * Aqui vamos usar Criteria API. Criteria é uma interface/API para criar
 		 * consultar dinâmicas e programáticas. Por sua burocracia, é preferível usá-la
@@ -48,37 +53,46 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 									 * composição das clausulas. Isso é feito por meio de seus métodos (Select, from
 									 * etc). Seu Generics define o tipo do resultado(consulta).
 									 */
-		/*Cláusulas são instruções dadas ao SQL, As mais comuns são SELECT, FROM e WHERE.
-		 * Já os elementos de uma Query são relacionando a atributos/colunas. São predicados.*/
-		Root<Restaurante> root = criteria.from(
-				Restaurante.class); /*
-									 * Root significa raiz. Refere-se a entidade que estamos realizando a consulta
-									 * (ou tabela, no modelo relacional). Podemos usar essa raiz para criar
-									 * predicados(filtros) de quais entidades queremos na Query, com qual atributo.
-									 */
-		Predicate nomePredicate = builder.like(root.get("nome"),
-				"%" + nome + "%"); /*
-									 * Um predicado é um critério de pesquisa, um filtro, construido por atributo
-									 * que queremos na Query. Nesse predicado, estamos definindo o critério de busca
-									 * (ou filtro) de um determinado atributo, no caso nome.
-									 */
-		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"),
-				taxaFreteInicial); /*
-									 * No primeiro parâmetro, definimos qual atributo de qual entidade queremos usar
-									 * na pesquisa. Ex: queremos fazer uma consulta de tal entidade (visto que uma
-									 * consulta pode ter várias entidades - tabelas, no modelo relacional -)com tal
-									 * atributo, criamos um predicado desse atributo, relacionando com outro. Nesse
-									 * segundo, definimos o valor que queremos relacionar, a partir da ação do
-									 * método. Ex: greaterThanOrEqualTo, like etc.
-									 */
-		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		/*
+		 * Cláusulas são instruções dadas ao SQL, As mais comuns são SELECT, FROM e
+		 * WHERE. Já os elementos de uma Query são relacionando a atributos/colunas. São
+		 * predicados.
+		 */
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		var predicates = new ArrayList<Predicate>();
+		/*
+		 * Root significa raiz. Refere-se a entidade que estamos realizando a consulta
+		 * (ou tabela, no modelo relacional). Podemos usar essa raiz para criar
+		 * predicados(filtros) de quais entidades queremos na Query, com qual atributo.
+		 */
+		if (StringUtils.hasLength(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		} /*
+			 * Um predicado é um critério de pesquisa, um filtro, construido por atributo
+			 * que queremos na Query. Nesse predicado, estamos definindo o critério de busca
+			 * (ou filtro) de um determinado atributo, no caso nome.
+			 */
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+
+		} /*
+			 * No primeiro parâmetro, definimos qual atributo de qual entidade queremos usar
+			 * na pesquisa. Ex: queremos fazer uma consulta de tal entidade (visto que uma
+			 * consulta pode ter várias entidades - tabelas, no modelo relacional -)com tal
+			 * atributo, criamos um predicado desse atributo, relacionando com outro. Nesse
+			 * segundo, definimos o valor que queremos relacionar, a partir da ação do
+			 * método. Ex: greaterThanOrEqualTo, like etc.
+			 */
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
 		/*
 		 * A partir do CriteriaBuilder nós criamos o CriteriaQuery. A partir do
 		 * CriteriaQuery nós criamos o Root, e a partir do Root, usando o
-		 * CriteriaBuilder criamos um Predicade.*/
+		 * CriteriaBuilder criamos um Predicade.
+		 */
 
-		criteria.where(nomePredicate, taxaInicialPredicate,
-				taxaFinalPredicate); /* O método Where recebe um ou mais predicados. */
+		criteria.where(predicates.toArray(new Predicate[0])); /* O método Where recebe um ou mais predicados. */
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		/*
 		 * Nessa sobrecarga, não é necessário definir o tipo da Query. Ele já está na
